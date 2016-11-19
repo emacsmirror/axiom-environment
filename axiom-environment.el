8e6faf7107dc242179cba782cc5438fe17f4616c
Paul Onions <paul.onions@acm.org>
2016-11-19 22:12:22 +0000

    Moved company-mode stuff to development branch.
    Until I find a way to test Emacs package builds before MELPA.

--- axiom-environment.el
+++ axiom-environment.el
@@ -51,8 +51,8 @@
 (require 'axiom-selector)
 
 ;; Load company-mode backend only if company-mode available
-(when (require 'company nil t)
-  (require 'axiom-company))
+;; (when (require 'company nil t)
+;;   (require 'axiom-company))
 
 ;; Automatically put .input, .spad and .boot files into the correct major mode.
 ;;;###autoload
